c6e69304c34cb60ea26ce7c0530fc53686078c1e
YdrMaster <ydrml@hotmail.com>
2022-08-12 15:24:30 +0800

    refactor(idx-allocator): 修改设计
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- idx-alloc/src/lib.rs
+++ idx-alloc/src/lib.rs
@@ -8,18 +8,19 @@ pub trait IdxAllocator {
     /// 在指定位置分配 `size`。
     fn allocate_fixed(&self, base: usize, size: usize) -> Result<(), AllocError>;
 
-    /// 分配一个对象，它对齐到它的 `size`。
-    #[inline]
-    fn allocate_sigle(&self, size: usize) -> Result<usize, AllocError> {
-        self.allocate(size, size)
-    }
-
-    /// 通常的分配，从一个对齐到 `align` 的位置分配 `size` 并返回位置。
+    /// 找到一个对齐到 `align` 的位置分配 `size` 并返回位置。
     fn allocate(&self, align: usize, size: usize) -> Result<usize, AllocError>;
 
     /// 回收一个对象。
     fn deallocate(&self, base: usize, size: usize);
 }
 
-/// 分配错误
-pub struct AllocError;
+/// 分配错误。
+pub enum AllocError {
+    /// 分配失败。找不到满足所有条件的空间。
+    Failed,
+    /// 不支持的分配要求。
+    ///
+    /// TODO 或许更好的方式是总结出一个特性空间，然后要求实现提供一个支持特性集常量，而不是返回不支持。
+    Unsupported,
+}
