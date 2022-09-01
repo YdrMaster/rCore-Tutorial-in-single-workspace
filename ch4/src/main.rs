0a3d3e7b0739bbeb1fa7df90bc588a3dc7e3aebe
YdrMaster <ydrml@hotmail.com>
2022-09-01 16:00:05 +0800

    fix(ch4): 堆从页帧借要对齐 2 的幂
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- ch4/src/main.rs
+++ ch4/src/main.rs
@@ -4,19 +4,27 @@
 #![feature(default_alloc_error_handler)]
 #![deny(warnings)]
 
+mod mm;
+
 #[macro_use]
 extern crate output;
 
 #[macro_use]
 extern crate alloc;
 
+use core::alloc::Layout;
+
 use ::page_table::{Sv39, VAddr};
+use alloc::vec::Vec;
 use impls::Console;
+use kernel_context::LocalContext;
 use kernel_vm::AddressSpace;
 use output::log;
 use page_table::{VmFlags, PPN};
 use riscv::register::satp;
 use sbi_rt::*;
+
+use crate::mm::PAGE;
 
 // 应用程序内联进来。
 core::arch::global_asm!(include_str!(env!("APP_ASM")));
@@ -57,65 +65,12 @@ extern "C" fn rust_main() -> ! {
     output::init_console(&Console);
     output::set_log_level(option_env!("LOG"));
     utils::test_log();
-    // 打印段位置
-    extern "C" {
-        fn __text();
-        fn __transit();
-        fn __rodata();
-        fn __data();
-        fn __end();
-    }
-    let _text = VAddr::<Sv39>::new(__text as _);
-    let _transit = VAddr::<Sv39>::new(__transit as _);
-    let _rodata = VAddr::<Sv39>::new(__rodata as _);
-    let _data = VAddr::<Sv39>::new(__data as _);
-    let _end = VAddr::<Sv39>::new(__end as _);
-    log::info!("__text ----> {:#10x}", _text.val());
-    log::info!("__transit -> {:#10x}", _transit.val());
-    log::info!("__rodata --> {:#10x}", _rodata.val());
-    log::info!("__data ----> {:#10x}", _data.val());
-    log::info!("__end -----> {:#10x}", _end.val());
-    println!();
+    // 初始化内核堆
     mm::init();
-
-    // 内核地址空间
-    let mut space = AddressSpace::<Sv39>::new(0);
-    space.push(
-        _text.floor().._transit.ceil(),
-        PPN::new(_text.floor().val()),
-        unsafe { VmFlags::from_raw(0b1011) },
-    );
-    space.push(
-        _transit.floor().._rodata.ceil(),
-        PPN::new(_transit.floor().val()),
-        unsafe { VmFlags::from_raw(0b1111) },
-    );
-    space.push(
-        _rodata.floor().._data.ceil(),
-        PPN::new(_rodata.floor().val()),
-        unsafe { VmFlags::from_raw(0b0011) },
-    );
-    space.push(
-        _data.floor().._end.ceil(),
-        PPN::new(_data.floor().val()),
-        unsafe { VmFlags::from_raw(0b0111) },
-    );
-    // println!("{:?}", space.shuttle().unwrap());
-    println!("page count = {:?}", space.page_count());
-    for seg in space.segments() {
-        println!("{seg}");
-    }
-    unsafe { satp::set(satp::Mode::Sv39, 0, space.root_ppn().unwrap().val()) };
-    // 测试内核堆分配
-    {
-        let mut vec = vec![0; 256];
-        for (i, val) in vec.iter_mut().enumerate() {
-            *val = i;
-        }
-        println!("{vec:?}");
-        println!();
-    }
-
+    mm::test();
+    // 建立内核地址空间
+    let _ks = kernel_space();
+    let mut processes = Vec::<Process>::new();
     {
         use xmas_elf::{
             header::{self, HeaderPt2, Machine},
@@ -138,7 +93,28 @@ extern "C" fn rust_main() -> ! {
                 {
                     continue;
                 }
-                for _segment in elf.program_iter() {}
+                let _address_space = AddressSpace::<Sv39>::new(0);
+                for program in elf.program_iter() {
+                    let off_file = program.offset();
+                    let end_file = off_file + program.file_size();
+                    let off_mem = program.virtual_addr();
+                    let end_mem = off_mem + program.mem_size();
+                    let svpn = VAddr::<Sv39>::new(off_mem as _).floor();
+                    let evpn = VAddr::<Sv39>::new(end_mem as _).ceil();
+                    let (_pages, size) = unsafe {
+                        PAGE.allocate_layout::<u8>(Layout::from_size_align_unchecked(
+                            (evpn.val() - svpn.val()) << 12,
+                            1 << 12,
+                        ))
+                        .unwrap()
+                    };
+                    assert_eq!(size, (evpn.val() - svpn.val()) << 12);
+                    println!("LOAD {off_file:#08x}..{end_file:#08x} -> {off_mem:#08x}..{end_mem:#08x} with {:?}", program.flags());
+                }
+                processes.push(Process {
+                    _context: LocalContext::user(pt2.entry_point as _),
+                    _address_space,
+                });
             }
         }
     }
@@ -168,80 +144,60 @@ mod impls {
     }
 }
 
-mod mm {
-    use alloc::alloc::handle_alloc_error;
-    use buddy_allocator::{BuddyAllocator, LinkedListBuddy, UsizeBuddy};
-    use core::{
-        alloc::{GlobalAlloc, Layout},
-        ptr::NonNull,
-    };
+/// 进程。
+struct Process {
+    _context: LocalContext,
+    _address_space: AddressSpace<Sv39>,
+}
 
-    /// 初始化全局分配器和内核堆分配器。
-    pub fn init() {
-        /// 4 KiB 页类型。
-        #[repr(C, align(4096))]
-        pub struct Pages<const N: usize>([u8; N]);
-
-        const MEMORY_SIZE: usize = 4 << 20;
-
-        /// 托管空间 4 MiB
-        static mut MEMORY: Pages<MEMORY_SIZE> = Pages([0u8; MEMORY_SIZE]);
-        unsafe {
-            let ptr = NonNull::new(MEMORY.0.as_mut_ptr()).unwrap();
-            println!(
-                "MEMORY = {:#x}..{:#x}",
-                ptr.as_ptr() as usize,
-                ptr.as_ptr() as usize + MEMORY_SIZE
-            );
-            PAGE.init(12, ptr);
-            HEAP.init(3, ptr);
-            PAGE.transfer(ptr, MEMORY_SIZE);
-            kernel_vm::init_allocator(&PageAllocator);
-        }
+fn kernel_space() -> AddressSpace<Sv39> {
+    // 打印段位置
+    extern "C" {
+        fn __text();
+        fn __transit();
+        fn __rodata();
+        fn __data();
+        fn __end();
     }
+    let _text = VAddr::<Sv39>::new(__text as _);
+    let _transit = VAddr::<Sv39>::new(__transit as _);
+    let _rodata = VAddr::<Sv39>::new(__rodata as _);
+    let _data = VAddr::<Sv39>::new(__data as _);
+    let _end = VAddr::<Sv39>::new(__end as _);
+    log::info!("__text ----> {:#10x}", _text.val());
+    log::info!("__transit -> {:#10x}", _transit.val());
+    log::info!("__rodata --> {:#10x}", _rodata.val());
+    log::info!("__data ----> {:#10x}", _data.val());
+    log::info!("__end -----> {:#10x}", _end.val());
+    println!();
 
-    type MutAllocator<const N: usize> = BuddyAllocator<N, UsizeBuddy, LinkedListBuddy>;
-    static mut PAGE: MutAllocator<5> = MutAllocator::new();
-    static mut HEAP: MutAllocator<22> = MutAllocator::new();
-
-    struct GlobAllocator;
-    struct PageAllocator;
-
-    impl kernel_vm::PageAllocator for PageAllocator {
-        #[inline]
-        fn allocate(&self, bits: usize) -> NonNull<u8> {
-            let size = 1 << bits;
-            unsafe { PAGE.allocate_layout(Layout::from_size_align_unchecked(size, size)) }
-                .unwrap()
-                .0
-        }
-
-        #[inline]
-        fn deallocate(&self, ptr: NonNull<u8>, bits: usize) {
-            unsafe { PAGE.deallocate(ptr, 1 << bits) };
-        }
+    // 内核地址空间
+    let mut space = AddressSpace::<Sv39>::new(0);
+    space.push(
+        _text.floor().._transit.ceil(),
+        PPN::new(_text.floor().val()),
+        unsafe { VmFlags::from_raw(0b1011) },
+    );
+    space.push(
+        _transit.floor().._rodata.ceil(),
+        PPN::new(_transit.floor().val()),
+        unsafe { VmFlags::from_raw(0b1111) },
+    );
+    space.push(
+        _rodata.floor().._data.ceil(),
+        PPN::new(_rodata.floor().val()),
+        unsafe { VmFlags::from_raw(0b0011) },
+    );
+    space.push(
+        _data.floor().._end.ceil(),
+        PPN::new(_data.floor().val()),
+        unsafe { VmFlags::from_raw(0b0111) },
+    );
+    // log::debug!("\n{:?}", space.shuttle().unwrap());
+    log::info!("kernel page count = {:?}", space.page_count());
+    for seg in space.segments() {
+        log::info!("{seg}");
     }
-
-    #[global_allocator]
-    static GLOBAL: GlobAllocator = GlobAllocator;
-
-    unsafe impl GlobalAlloc for GlobAllocator {
-        #[inline]
-        unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-            loop {
-                if let Ok((ptr, _)) = HEAP.allocate_layout::<u8>(layout) {
-                    return ptr.as_ptr();
-                } else if let Ok((ptr, size)) = PAGE.allocate_layout::<u8>(layout) {
-                    HEAP.transfer(ptr, size);
-                } else {
-                    handle_alloc_error(layout)
-                }
-            }
-        }
-
-        #[inline]
-        unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-            HEAP.deallocate(NonNull::new(ptr).unwrap(), layout.size())
-        }
-    }
+    unsafe { satp::set(satp::Mode::Sv39, 0, space.root_ppn().unwrap().val()) };
+    space
 }
