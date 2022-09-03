abb45889b4403dbe3b801bfcf1fe80f11da7e04d
YdrMaster <ydrml@hotmail.com>
2022-09-03 08:10:50 +0800

    feat(kernel-vm): 简化地址空间设计，增加查询地址空间上指针的功能
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- kernel-vm/src/space/mapper.rs
+++ kernel-vm/src/space/mapper.rs
@@ -1,15 +1,16 @@
-﻿use super::{AddressSpace, Page, Table};
+﻿use super::Page;
 use core::ops::Range;
 use page_table::{Decorator, Pos, Pte, Update, VmFlags, VmMeta, PPN, VPN};
 
-pub(super) struct Mapper<'a, Meta: VmMeta> {
-    pub space: &'a mut AddressSpace<Meta>,
+pub(super) struct Mapper<Meta: VmMeta> {
+    pub vpn_offset: usize,
     pub vbase: VPN<Meta>,
     pub prange: Range<PPN<Meta>>,
     pub flags: VmFlags<Meta>,
 }
 
-impl<'a, Meta: VmMeta> Decorator<Meta> for Mapper<'a, Meta> {
+impl<Meta: VmMeta> Decorator<Meta> for Mapper<Meta> {
+    #[inline]
     fn start(&mut self, _pos: Pos<Meta>) -> Pos<Meta> {
         Pos {
             vpn: self.vbase,
@@ -17,6 +18,7 @@ impl<'a, Meta: VmMeta> Decorator<Meta> for Mapper<'a, Meta> {
         }
     }
 
+    #[inline]
     fn arrive(&mut self, pte: &mut Pte<Meta>, target_hint: Pos<Meta>) -> Pos<Meta> {
         assert!(!pte.is_valid());
         *pte = self.flags.build_pte(self.prange.start);
@@ -28,15 +30,12 @@ impl<'a, Meta: VmMeta> Decorator<Meta> for Mapper<'a, Meta> {
         }
     }
 
-    fn meet(&mut self, level: usize, pte: Pte<Meta>, target_hint: Pos<Meta>) -> Update<Meta> {
+    #[inline]
+    fn meet(&mut self, _level: usize, pte: Pte<Meta>, _target_hint: Pos<Meta>) -> Update<Meta> {
         assert!(!pte.is_valid());
         let page = Page::<Meta>::allocate();
         let vpn = page.vpn();
-        let ppn = PPN::new(vpn.val() - self.space.vpn_offset);
-        self.space.tables[level - 1].insert(
-            target_hint.vpn.val() >> Meta::LEVEL_BITS[..level].iter().sum::<usize>(),
-            Table(page, 0),
-        );
+        let ppn = PPN::new(vpn.val() - self.vpn_offset);
         Update::Pte(unsafe { VmFlags::from_raw(1) }.build_pte(ppn), vpn)
     }
 }
